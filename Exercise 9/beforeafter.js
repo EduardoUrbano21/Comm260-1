7c6d877e1d5627f350f047a456866e690eebfbd2
EduardoUrbano21 <43761108+EduardoUrbano21@users.noreply.github.com>
2019-03-31 20:26:03 -0500

    Add files via upload

---
+++ Exercise 9/beforeafter.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+	$("#after img").each(function() {
+
+		var oldURL = $(this).attr("src");
+		var newURL = $(this).attr("id");
+			
+		var rolloverImage = new Image();
+		rolloverImage.src = newURL;
+					
+		$(this).hover(
+			function() {
+				$(this).attr("src", newURL);
+			},
+			function() {
+				$(this).attr("src", oldURL);
+			}
+		);
+	});
+}); 
