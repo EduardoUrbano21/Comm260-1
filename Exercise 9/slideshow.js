1bac05a38620ce426dec5ab0ee3a1a01541528bc
EduardoUrbano21 <43761108+EduardoUrbano21@users.noreply.github.com>
2019-03-31 20:30:14 -0500

    Update slideshow.js

--- Exercise 9/slideshow.js
+++ Exercise 9/slideshow.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	var nextSlide = $("#slides img:first-child");
+	var nextCaption;
 	var nextSlideSource;
 	
 	// the function for running the slide show	
 	var runSlideShow = function() {   
-        //$("#caption").fadeOut(3000);
+        $("#caption").fadeOut(3000);
         $("#slide").fadeOut(3000,
         	function () {
        	     	if (nextSlide.next().length == 0) {
@@ -14,8 +15,9 @@ $(document).ready(function() {
 					nextSlide = nextSlide.next();
 				}
 				nextSlideSource = nextSlide.attr("src");
+				nextCaption = nextSlide.attr("alt"); 
 				$("#slide").attr("src", nextSlideSource).fadeIn(3000);					
-				
+				$("#caption").text(nextCaption).fadeIn(3000);
 			}
 		)
 	}
@@ -23,6 +25,20 @@ $(document).ready(function() {
 	// start slide show
     var timer1 = setInterval(runSlideShow, 3000);
     
+    // start and stop the slide show
+	// but the toggle event method has been removed from jQuery 1.9
+	/*
+	$("#slide").toggle( 
+		function() {
+		    clearInterval(timer1);
+		},
+		function() {
+			timer1 = setInterval(runSlideShow, 3000);
+		}
+	)
+	*/
+	
+	// here's one way to code this app without using the toggle event method
 	$("#slide").click(function() {
 		if (timer1 != null) {
 	    	clearInterval(timer1);
